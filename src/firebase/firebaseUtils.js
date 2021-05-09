9dee07197186ad9f10eff12b2bbb96a63dfa7188
dhruw <dhruwlalan22@gmail.com>
2021-05-09 19:39:55 +0530

    Initial Commit

---
+++ src/firebase/firebaseUtils.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/storage';
+import firebaseConfig from './firebaseConfig';
+
+///Initialize Firebase///
+firebase.initializeApp(firebaseConfig);
+
+///Utils///
+const fb = firebase;
+const auth = firebase.auth();
+const storageRef = firebase.storage().ref();
+
+export { fb, auth, storageRef };
