858639bc07dfc098b3842e74d43cbf05db98fc1c
dhruw <dhruwlalan22@gmail.com>
2021-05-12 12:16:21 +0530

    implemented profile photo zoom in-out

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import Navbar from '../components/layout/Navbar';
 import Pipes from '../components/layout/Pipes';
+import BodyPortal from '../components/utils/BodyPortal';
 import { selectCurrentUser } from '../redux/user/userSelector';
 
 const Dashboard = ({ user }) => {
+   const [viewProfilePhoto, setViewProfilePhoto] = useState(false);
+   const nodeRef = useRef(null);
+
+   const openPhoto = () => {
+      setViewProfilePhoto(true);
+      const node = nodeRef.current;
+      node.classList.remove('profile-zoom-out');
+      node.classList.remove('home__profile--photo');
+      node.classList.add('home__profile--view-photo');
+      node.classList.add('profile-zoom-in');
+   };
+   const closePhoto = () => {
+      setViewProfilePhoto(false);
+      const node = nodeRef.current;
+      node.classList.remove('profile-zoom-in');
+      node.classList.add('profile-zoom-out');
+      setTimeout(() => {
+         node.classList.add('home__profile--photo');
+      }, 200);
+   };
+
    return (
       <>
+         <BodyPortal>
+            <div
+               className="home__profile--backdrop"
+               style={{ display: viewProfilePhoto ? 'block' : 'none' }}
+               onClick={closePhoto}
+            ></div>
+         </BodyPortal>
          <Navbar page="home" />
          <Pipes />
          <div className="section">
             <h1 className="home__title">Welcome!</h1>
-            <div className="home__profile">
-               {/* <img className="home__profile--view-photo" />
-               <img className="home__profile--photo" v-if="true" /> */}
+            <div className="home__profile" onClick={openPhoto}>
+               <img
+                  className="home__profile--photo"
+                  ref={nodeRef}
+                  src={user.photoURL}
+                  alt="profile"
+               />
             </div>
          </div>
          <Pipes />
